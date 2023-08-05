5dd3d5d05ab9a8d66033293076c754bff6dd232c
Galact07 <sidharthnair1907@gmail.com>
2023-08-05 13:04:14 +0530

    changes

--- src/app/components/FriendRequest.tsx
+++ src/app/components/FriendRequest.tsx
@@ -46,7 +46,7 @@ const FriendRequest: FC<FriendRequestProps> = ({sessionId,initialFriendRequests}
     <div className='text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'>
       <User className='h-4 w-4' />
     </div>
-    <p className='truncate'>Friend requests</p>
+    <p className='truncate'>Friend requests Received</p>
     {friendRequests>0&&<span className='rounded-full w-5 h-5 text-xs flex justify-center items-center text-white bg-indigo-600'>{friendRequests}</span>
     }
   </Link>
